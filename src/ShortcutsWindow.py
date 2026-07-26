7637550cc5aaeda69f64ad6432ddf81db2597e58
Nicholas La Roux <larouxn@gmail.com>
2026-07-26 13:04:14 +0200

    Migrate from deprecated GtkShortcutsWindow to modern AdwShortcutsDialog

--- src/ShortcutsWindow.py
+++ src/ShortcutsWindow.py
@@ -7,15 +7,16 @@ import re
 from .assets.emoji_list import emojis
 
 gi.require_version('Gtk', '4.0')
+gi.require_version('Adw', '1')
 
-from gi.repository import Gtk, Gio, Gdk  # noqa
+from gi.repository import Gtk, Gio, Gdk, Adw  # noqa
 
 class ShortcutsWindow():
     def __init__(self):
+        Adw.init()
         builder = Gtk.Builder()
         builder.add_from_resource('/it/mijorus/smile/ui/shortcuts.ui')
         self.shortcut_window = builder.get_object('shortcuts')
-        self.shortcut_window.set_default_size(600, 400)
 
         settings = Gio.Settings.new('it.mijorus.smile')
 
@@ -23,8 +24,9 @@ class ShortcutsWindow():
         copy_quit_label = _('Copy the selected emoji and hide the window')
 
         mouse_multi_select = settings.get_boolean('mouse-multi-select')
-        builder.get_object('shift-left-click-label').set_label(copy_quit_label if mouse_multi_select else add_em_to_selection_label)
-        builder.get_object('left-click-label').set_label(add_em_to_selection_label if mouse_multi_select else copy_quit_label)
+        builder.get_object('shift-left-click-item').set_title(copy_quit_label if mouse_multi_select else add_em_to_selection_label)
+        builder.get_object('left-click-item').set_title(add_em_to_selection_label if mouse_multi_select else copy_quit_label)
 
-    def open(self):
-        self.shortcut_window.present()
+    def open(self, parent=None):
+        self.shortcut_window.present(parent)
+
